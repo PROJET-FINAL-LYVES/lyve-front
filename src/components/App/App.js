3d93783e8cee0b9b6f223030dff34706b54c1874
Sam <59567331+Samz87@users.noreply.github.com>
2023-03-07 13:52:56 +0100

    Add react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import HomeComponent from '../Home/HomeComponent';
+import LoginComponent from '../Login/LoginComponent';
+
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <p className="text-3xl text-gray-700 font-bold mb-5">
-          Hello lyve !
-        </p>
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomeComponent />} />
+      <Route path="/login" element={<LoginComponent />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Home/HomeComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const HomeComponent = () => (
+    <>
+        <h1>Home</h1>
+    </>
+);
+
+export default HomeComponent;
